c0d30ea9d726e4002071bd14e606cbd7ffde7b52
GabriRDiaz <correoclasegabri@gmail.com>
2020-01-29 20:28:52 +0100

    MenuBar + Action Listeners

--- src/org/liceolapaz/des/Ventana.java
+++ src/org/liceolapaz/des/Ventana.java
@@ -1,23 +1,102 @@
 package org.liceolapaz.des;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 import javax.swing.border.LineBorder;
 
 public class Ventana extends JFrame {
 	public Ventana() {
-		super();
-		setTitle("Documento nuevo");
+		super("Documento nuevo");
 		setSize(1024, 768);
 		setLocationRelativeTo(null);
 		setResizable(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(Color.YELLOW, 5));
-		panel.setBackground(Color.BLACK);
-		add(panel);
+		crearBarra();
 	}
 	
+	private void crearBarra() {
+		JMenuBar barraMenu = new JMenuBar();
+		add(barraMenu);
+		//ARCHIVO
+		JMenu menuArchivo = new JMenu("Archivo");
+		menuArchivo.setMnemonic(KeyEvent.VK_P);
+		barraMenu.add(menuArchivo);
+		
+		//Archivo --> Nuevo
+		JMenuItem archivoNuevo = new JMenuItem("Nuevo");
+		archivoNuevo.setMnemonic(KeyEvent.VK_N);
+		archivoNuevo.setAccelerator(KeyStroke.getKeyStroke("ctrl N"));
+		menuArchivo.add(archivoNuevo);
+		
+		//Archivo --> Abrir
+		JMenuItem archivoAbrir = new JMenuItem("Abrir");
+		archivoAbrir.setMnemonic(KeyEvent.VK_G);
+		archivoAbrir.setAccelerator(KeyStroke.getKeyStroke("ctrl O"));
+		menuArchivo.add(archivoAbrir);
+		
+		//Archivo --> Guardar
+		JMenuItem archivoGuardar = new JMenuItem("Guardar");
+		archivoGuardar.setMnemonic(KeyEvent.VK_S);
+		archivoGuardar.setAccelerator(KeyStroke.getKeyStroke("ctrl S"));
+		menuArchivo.add(archivoGuardar);
+		
+		//Archivo --> Guardar como
+		JMenuItem archivoGuardarAs = new JMenuItem("Guardar como");
+		archivoGuardarAs.setMnemonic(KeyEvent.VK_G);
+		archivoGuardarAs.setAccelerator(KeyStroke.getKeyStroke("ctrl shift S"));
+		menuArchivo.add(archivoGuardarAs);
+		
+		//Archivo --> Salir
+		JMenuItem archivoSalir = new JMenuItem("Salir");
+		archivoSalir.setMnemonic(KeyEvent.VK_L);
+		archivoSalir.setAccelerator(KeyStroke.getKeyStroke("ctrl L"));
+		menuArchivo.add(archivoSalir);
+		//Action Listeners
+		archivoSalir.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);				
+			}
+		});
+		archivoGuardarAs.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Funciona");
+			}
+		});
+		archivoGuardar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Funciona");
+			}
+		});
+		archivoAbrir.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Funciona");
+			}
+		});
+		archivoNuevo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Funciona");
+			}
+		
+		});
+		setJMenuBar(barraMenu);
+}
 }
